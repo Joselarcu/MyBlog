851124444291fb6c89f293227073aaafc75bbc25
José Vicente Lara Cuadrado <jose.lara.cuadrado@gmail.com>
2014-12-17 19:10:24 +0100

    add gravatar to user

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -10,4 +10,9 @@ module CommentsHelper
   def find_user(id)
     User.find(id)
   end
+
+  def avatar_url(user, size)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  end
 end
